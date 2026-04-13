8d3f9d0d1e0c7d26ff525517588d42c1fbb7261f
unknown <kartiks04800@gmail.com>
2026-04-13 18:46:35 +0530

    more_changes

--- sbiyono/src/main/java/jwt/sbiyono/config/SecurityConfig.java
+++ sbiyono/src/main/java/jwt/sbiyono/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                        .requestMatchers("/", "/api/auth/register", "/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
